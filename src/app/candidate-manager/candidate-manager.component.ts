0ef5bfa25c4619a3fa862067a75f364f803ab8b9
Ismael Illanes <iiv94@hotmail.com>
2025-02-22 12:29:23 +0100

    fix in pagination and comments added

--- src/app/candidate-manager/candidate-manager.component.ts
+++ src/app/candidate-manager/candidate-manager.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, ChangeDetectorRef, ChangeDetectionStrategy, ViewChild, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectorRef,
+  ChangeDetectionStrategy,
+  ViewChild,
+  AfterViewInit,
+} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CandidateService } from '../../services/CandidateService';
 import { Candidate } from '../../models/Candidate.model';
@@ -33,21 +40,45 @@ import { CandidateEditDialogComponent } from '../candidate-edit-dialog/candidate
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CandidateManagerComponent implements OnInit, AfterViewInit {
-  candidateForm: FormGroup;
+  candidateForm!: FormGroup;
   candidates: Candidate[] = [];
   dataSource = new MatTableDataSource<Candidate>([]);
-  displayedColumns: string[] = ['name', 'surname', 'seniority', 'years', 'availability'];
+  displayedColumns: string[] = [
+    'name',
+    'surname',
+    'seniority',
+    'years',
+    'availability',
+  ];
   displayedColumnsWithActions: string[] = [...this.displayedColumns, 'actions'];
   excelData: any;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
+  // Constructor: injects dependencies and initializes the form.
   constructor(
     private fb: FormBuilder,
     private candidateService: CandidateService,
     private cdr: ChangeDetectorRef,
     public dialog: MatDialog
   ) {
+    this.initializeForm();
+  }
+
+  // ngOnInit: loads candidates when the component initializes.
+  ngOnInit(): void {
+    this.loadCandidates();
+  }
+
+  // ngAfterViewInit: assigns the paginator to the dataSource after the view is rendered.
+  ngAfterViewInit() {
+    if (this.paginator) {
+      this.dataSource.paginator = this.paginator;
+    }
+  }
+
+  // Initializes the reactive form.
+  initializeForm(): void {
     this.candidateForm = this.fb.group({
       name: ['', Validators.required],
       surname: ['', Validators.required],
@@ -55,124 +86,148 @@ export class CandidateManagerComponent implements OnInit, AfterViewInit {
     });
   }
 
-  ngOnInit(): void {
-    this.loadCandidates();
+  // Loads candidates from the backend.
+  loadCandidates(): void {
+    this.candidateService.getAllCandidates().subscribe((data: Candidate[]) => {
+      console.log('Candidates loaded:', data);
+      this.candidates = data;
+      this.updateDataSource();
+    });
   }
 
-  ngAfterViewInit() {
+  // Updates the table dataSource and assigns the paginator.
+  updateDataSource(): void {
+    console.log('Updating dataSource:', this.candidates);
+    this.sortCandidates();
+    this.dataSource = new MatTableDataSource<Candidate>(this.candidates);
     setTimeout(() => {
       if (this.paginator) {
         this.dataSource.paginator = this.paginator;
         this.paginator.firstPage();
       }
-    });
+    }, 0);
+    this.cdr.detectChanges();
   }
 
-  loadCandidates(): void {
-    this.candidateService.getAllCandidates().subscribe((data: Candidate[]) => {
-      this.candidates = data;
-      this.sortCandidates();
-      this.dataSource.data = this.candidates;
-      this.cdr.detectChanges();
-    });
-  }
-
+  // Sorts candidates by their id.
   sortCandidates(): void {
     this.candidates.sort((a, b) => (a.id ?? 0) - (b.id ?? 0));
   }
 
+  // Handles the change event in the file input.
   onFileChange(event: any) {
     const file = event.target.files[0];
     if (file) {
-      if (file.type !== 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
-        alert('Por favor, selecciona solo archivos Excel (.xlsx).');
-        this.candidateForm.get('excelFile')?.setValue(null);
-
-        const fileInput = document.querySelector('input[type="file"]') as HTMLInputElement;
-        if (fileInput) {
-          fileInput.value = '';
-        }
+      if (!this.isValidExcelFile(file)) {
+        this.resetFileInput();
         return;
       }
-      const reader = new FileReader();
-      reader.onload = (e: any) => {
-        const data = e.target.result;
-        const workbook = XLSX.read(data, { type: 'binary' });
-        const sheetName = workbook.SheetNames[0];
-        const worksheet = workbook.Sheets[sheetName];
-        const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-        if (jsonData.length > 1) {
-          this.excelData = jsonData[1];
-          this.candidateForm.patchValue({ excelFile: this.excelData });
-        }
-      };
-      reader.readAsBinaryString(file);
+      this.readExcelFile(file);
     }
   }
 
-  onSubmit() {
-    if (this.candidateForm.valid) {
-      const formValues = this.candidateForm.value;
-      const formData = new FormData();
-      formData.append('name', formValues.name);
-      formData.append('surname', formValues.surname);
-
-      const fileInput = document.querySelector('input[type="file"]') as HTMLInputElement;
-      if (fileInput && fileInput.files && fileInput.files.length > 0) {
-        formData.append('file', fileInput.files[0]);
-      }
-
-      this.candidateService.submitCandidate(formData).subscribe((response: Candidate) => {
-        this.candidates.push(response);
-        this.sortCandidates();
-        this.dataSource.data = this.candidates;
-        this.candidateForm.reset();
-        this.excelData = null;
-        this.clearFileInput();
-        this.cdr.detectChanges();
-      });
+  // Validates that the file is an Excel (.xlsx).
+  isValidExcelFile(file: File): boolean {
+    if (
+      file.type !==
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    ) {
+      alert('Please select only Excel files (.xlsx).');
+      this.candidateForm.get('excelFile')?.setValue(null);
+      return false;
     }
+    return true;
   }
 
-  clearFileInput() {
-    const fileInput = document.querySelector('input[type="file"]') as HTMLInputElement;
+  // Resets the file input.
+  resetFileInput(): void {
+    const fileInput = document.querySelector(
+      'input[type="file"]'
+    ) as HTMLInputElement;
     if (fileInput) {
       fileInput.value = '';
     }
   }
 
+  // Reads and processes the content of the Excel file.
+  readExcelFile(file: File): void {
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      const data = e.target.result;
+      const workbook = XLSX.read(data, { type: 'binary' });
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+      const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+      if (jsonData.length > 1) {
+        this.excelData = jsonData[1];
+        this.candidateForm.patchValue({ excelFile: this.excelData });
+      }
+    };
+    reader.readAsBinaryString(file);
+  }
+
+  // Submits the form and updates the candidate list.
+  onSubmit() {
+    if (this.candidateForm.valid) {
+      const formData = this.createFormData();
+      this.candidateService
+        .submitCandidate(formData)
+        .subscribe((response: Candidate) => {
+          this.candidates.push(response);
+          this.updateDataSource();
+          this.resetForm();
+        });
+    }
+  }
+
+  // Creates a FormData object from the form values.
+  createFormData(): FormData {
+    const formValues = this.candidateForm.value;
+    const formData = new FormData();
+    formData.append('name', formValues.name);
+    formData.append('surname', formValues.surname);
+    const fileInput = document.querySelector(
+      'input[type="file"]'
+    ) as HTMLInputElement;
+    if (fileInput && fileInput.files && fileInput.files.length > 0) {
+      formData.append('file', fileInput.files[0]);
+    }
+    return formData;
+  }
+
+  // Resets the form and the file input.
+  resetForm(): void {
+    this.candidateForm.reset();
+    this.excelData = null;
+    this.resetFileInput();
+  }
+
+  // Opens a dialog to edit a candidate.
   editCandidate(candidate: Candidate): void {
     const dialogRef = this.dialog.open(CandidateEditDialogComponent, {
       width: '400px',
       data: candidate,
     });
-
     dialogRef.afterClosed().subscribe((result) => {
       if (result) {
         const updatedCandidate: Candidate = { ...candidate, ...result };
-        this.candidateService.updateCandidate(updatedCandidate).subscribe(() => {
-          this.loadCandidates();
-        });
+        this.candidateService
+          .updateCandidate(updatedCandidate)
+          .subscribe(() => {
+            this.loadCandidates();
+          });
       }
     });
   }
 
+  // Deletes a candidate and updates the table.
   deleteCandidate(id: number): void {
     this.candidateService.deleteCandidate(id).subscribe(() => {
-      this.candidates = this.candidates.filter((candidate) => candidate.id !== id);
-      this.sortCandidates();
-      this.dataSource.data = this.candidates;
-      this.resetPaginator();
-      this.cdr.detectChanges();
-    });
-  }
-
-  resetPaginator() {
-    setTimeout(() => {
-      if (this.paginator) {
-        this.dataSource.paginator = this.paginator;
-        this.paginator.firstPage();
-      }
+      this.candidates = this.candidates.filter(
+        (candidate) => candidate.id !== id
+      );
+      this.updateDataSource();
+      this.paginator.firstPage();
     });
   }
 }
