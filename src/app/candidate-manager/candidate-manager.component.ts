2c5a36883e12e970cbe061cb12a2198eced4de75
Ismael Illanes <iiv94@hotmail.com>
2025-02-22 17:17:26 +0100

    comments add

--- src/app/candidate-manager/candidate-manager.component.ts
+++ src/app/candidate-manager/candidate-manager.component.ts
@@ -68,16 +68,19 @@ export class CandidateManagerComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit(): void {
+    // Load candidates on component initialization.
     this.loadCandidates();
   }
 
   ngAfterViewInit() {
+    // Set paginator after view initialization.
     if (this.paginator) {
       this.dataSource.paginator = this.paginator;
     }
   }
 
   initializeForm(): void {
+    // Initialize the form group.
     this.candidateForm = this.fb.group({
       name: ['', [Validators.required, Validators.maxLength(50)]],
       surname: ['', [Validators.required, Validators.maxLength(50)]],
@@ -86,6 +89,7 @@ export class CandidateManagerComponent implements OnInit, AfterViewInit {
   }
 
   loadCandidates(): void {
+    // Load candidates from the service.
     this.loading = true;
     this.candidateService
       .getAllCandidates()
@@ -97,20 +101,18 @@ export class CandidateManagerComponent implements OnInit, AfterViewInit {
       )
       .subscribe({
         next: (data: Candidate[]) => {
-          console.log('Candidates loaded:', data);
           this.candidates = data;
           this.updateDataSource();
           this.serverError = false;
         },
         error: (error) => {
-          console.error('Error loading candidates:', error);
           this.serverError = true;
         },
       });
   }
 
   updateDataSource(): void {
-    console.log('Updating dataSource:', this.candidates);
+    // Update the data source with the candidates.
     this.sortCandidates();
     this.dataSource = new MatTableDataSource<Candidate>(this.candidates);
     setTimeout(() => {
@@ -123,10 +125,12 @@ export class CandidateManagerComponent implements OnInit, AfterViewInit {
   }
 
   sortCandidates(): void {
+    // Sort candidates by ID.
     this.candidates.sort((a, b) => (a.id ?? 0) - (b.id ?? 0));
   }
 
   onFileChange(event: any) {
+    // Handle file change event.
     const file = event.target.files[0];
     if (file) {
       if (!this.isValidExcelFile(file)) {
@@ -138,6 +142,7 @@ export class CandidateManagerComponent implements OnInit, AfterViewInit {
   }
 
   isValidExcelFile(file: File): boolean {
+    // Validate if the file is an Excel file.
     if (
       file.type !==
       'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
@@ -150,6 +155,7 @@ export class CandidateManagerComponent implements OnInit, AfterViewInit {
   }
 
   resetFileInput(): void {
+    // Reset the file input.
     const fileInput = document.querySelector(
       'input[type="file"]'
     ) as HTMLInputElement;
@@ -159,6 +165,7 @@ export class CandidateManagerComponent implements OnInit, AfterViewInit {
   }
 
   readExcelFile(file: File): void {
+    // Read and parse the Excel file.
     const reader = new FileReader();
     reader.onload = (e: any) => {
       const data = e.target.result;
@@ -169,16 +176,22 @@ export class CandidateManagerComponent implements OnInit, AfterViewInit {
 
       const headers: string[] = jsonData[0] as string[];
       const requiredHeaders = ['seniority', 'years', 'availability'];
-      const hasValidHeaders = requiredHeaders.every(header => headers.includes(header));
+      const hasValidHeaders = requiredHeaders.every((header) =>
+        headers.includes(header)
+      );
 
       if (!hasValidHeaders) {
-        alert('El archivo Excel debe contener los encabezados: seniority, years, availability.');
+        alert(
+          'El archivo Excel debe contener los encabezados: seniority, years, availability.'
+        );
         this.resetFileInput();
         return;
       }
 
       if (jsonData.length !== 2) {
-        alert('El archivo Excel debe contener solo una fila de datos además de la fila de encabezados.');
+        alert(
+          'El archivo Excel debe contener solo una fila de datos además de la fila de encabezados.'
+        );
         this.resetFileInput();
         return;
       }
@@ -190,6 +203,7 @@ export class CandidateManagerComponent implements OnInit, AfterViewInit {
   }
 
   onSubmit() {
+    // Submit the candidate form.
     if (this.candidateForm.valid) {
       const formData = this.createFormData();
       this.candidateService
@@ -207,7 +221,6 @@ export class CandidateManagerComponent implements OnInit, AfterViewInit {
             this.resetForm();
           },
           error: (error) => {
-            console.error('Error submitting candidate:', error);
             this.serverError = true;
           },
         });
@@ -215,6 +228,7 @@ export class CandidateManagerComponent implements OnInit, AfterViewInit {
   }
 
   createFormData(): FormData {
+    // Create form data for submission.
     const formValues = this.candidateForm.value;
     const formData = new FormData();
     formData.append('name', formValues.name);
@@ -229,12 +243,14 @@ export class CandidateManagerComponent implements OnInit, AfterViewInit {
   }
 
   resetForm(): void {
+    // Reset the candidate form.
     this.candidateForm.reset();
     this.excelData = null;
     this.resetFileInput();
   }
 
   editCandidate(candidate: Candidate): void {
+    // Open the edit candidate dialog.
     const dialogRef = this.dialog.open(CandidateEditDialogComponent, {
       width: '400px',
       data: candidate,
@@ -255,35 +271,34 @@ export class CandidateManagerComponent implements OnInit, AfterViewInit {
               this.loadCandidates();
             },
             error: (error) => {
-              console.error('Error updating candidate:', error);
               this.serverError = true;
             },
           });
+      }
+    });
   }
-});
-}
 
-deleteCandidate(id: number): void {
-this.candidateService
-.deleteCandidate(id)
-.pipe(
-    finalize(() => {
-      this.loading = false;
-      this.cdr.detectChanges();
-    })
-  )
-.subscribe({
-    next: () => {
-      this.candidates = this.candidates.filter(
-        (candidate) => candidate.id!== id
-      );
-      this.updateDataSource();
-      this.paginator.firstPage();
-    },
-    error: (error) => {
-      console.error('Error deleting candidate:', error);
-      this.serverError = true;
-    },
-  });
-}
+  deleteCandidate(id: number): void {
+    // Delete a candidate by ID.
+    this.candidateService
+      .deleteCandidate(id)
+      .pipe(
+        finalize(() => {
+          this.loading = false;
+          this.cdr.detectChanges();
+        })
+      )
+      .subscribe({
+        next: () => {
+          this.candidates = this.candidates.filter(
+            (candidate) => candidate.id !== id
+          );
+          this.updateDataSource();
+          this.paginator.firstPage();
+        },
+        error: (error) => {
+          this.serverError = true;
+        },
+      });
+  }
 }
